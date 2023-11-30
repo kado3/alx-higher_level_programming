d0262283506217739665797e56517ce51a148520
kado3 <k.o360@live.com>
2023-11-29 21:47:28 -0800

    first commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-strtmp = ""
-for i in reversed(range(97, 123)):
-    if (i % 2) == 0:
-        strtmp += chr(i)
-    else:
-        strtmp += chr(i-32)
-print("{}".format(strtmp), end="")
+for ch in reversed(range(97, 123)):
+    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
